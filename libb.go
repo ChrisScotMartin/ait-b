deb6148f2277302313de2b374b981076068c5d38
Chris Martin <101210272+ChrisScotMartin@users.noreply.github.com>
2022-11-16 14:20:44 -0500

    changed println

--- libb.go
+++ libb.go
@@ -8,6 +8,6 @@ import (
 // ait-b imports pkg/mod from  ait-c
 
 func SomeLibraryFunc() {
-	fmt.Println("I'm a func from the main root module ait-a")
+	fmt.Println("I'm a func from the lib function in pkgbodb")
 	pkgmodc.Hello()
 }
